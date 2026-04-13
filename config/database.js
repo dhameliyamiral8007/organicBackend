1845f2163bc5691a81af6c2ec2765a83459922d8
dhameliyamiral8007 <dhameliyamiral8007@gmail.com>
2026-04-13 23:42:01 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -3,12 +3,18 @@ const { Client } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
+const useSSL = String(process.env.DB_SSL || "").toLowerCase() === "true";
+const rejectUnauthorized = String(process.env.DB_SSL_REJECT_UNAUTHORIZED || "false").toLowerCase() === "true";
+
 const client = new Client({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: useSSL
+    ? { rejectUnauthorized }
+    : false,
 });
 
 // Connect to database
